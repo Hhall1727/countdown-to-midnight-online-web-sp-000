6603ed3531cd3d3ebdd199c9deb3461649e4d8a2
Halie Hall <halierae17@outlook.com>
2021-04-12 19:43:23 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
 #write your code here
 
-def countdown
+def countdown(n)
+  n = 10
+  while n > 0 do
+    puts "#{n} SECOND(S)!"
+    n -= 1 
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+
+def countdown_with_sleep(runtime)
+  runtime.times do
+    break if runtime >= 5
+    sleep(5.seconds)
+    return true 
+  end
 end
